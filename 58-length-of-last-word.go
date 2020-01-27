1fa2512934509bc96bd31b64864281abd48cf02e
redeyes420420 <58033756+redeyes420420@users.noreply.github.com>
2020-01-27 15:04:16 +0500

    Create 58-length-of-last-word.go

---
+++ 58-length-of-last-word.go
@@ -0,0 +1,10 @@
+func lengthOfLastWord(s string) int {
+    
+    words := strings.Split(strings.TrimSpace(s), " ")
+    
+    if len(words) == 0 {
+        return 0
+    }
+
+    return len(words[len(words)-1])
+}
